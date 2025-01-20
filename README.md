3aa31510efebb8c2b5d4fac7ab50884e36ab9a0a
uma-victor1 <umavictor11@gmail.com>
2025-01-20 01:44:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+    <a href="https://umavictor.com"><h1 align="center">umavictor.com</h1></a>
+
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com?ref=chronark.com), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+## Running Locally
+
+```sh-session
+git clone this repo
+cd <into the cloned repo>
+```
+
+Create a `.env` file similar to `.env.example`
+
+Then install dependencies and run the development server:
+
+```sh-session
+pnpm install
+pnpm dev
+```
+
+## Cloning / Forking
+
+Please remove all of my personal information (projects, images, etc.) before deploying your own version of this site.
---
+++ app/projects/page.tsx
@@ -0,0 +1,82 @@
+import Link from "next/link";
+import React from "react";
+import { allProjects } from "contentlayer/generated";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+import { Article } from "./article";
+import { Redis } from "@upstash/redis";
+import { Eye } from "lucide-react";
+
+const redis = Redis.fromEnv();
+
+export const revalidate = 60;
+export default async function ProjectsPage() {
+  const views = (
+    await redis.mget<number[]>(
+      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":"))
+    )
+  ).reduce((acc, v, i) => {
+    acc[allProjects[i].slug] = v ?? 0;
+    return acc;
+  }, {} as Record<string, number>);
+
+  console.log(allProjects, "allprojects");
+
+  const sorted = allProjects
+    .filter((p) => p.published)
+
+    .sort(
+      (a, b) =>
+        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
+    );
+
+  return (
+    <div className="relative pb-16">
+      <Navigation />
+      <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+        <div className="max-w-2xl mx-auto lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+            Projects
+          </h2>
+          <p className="mt-4 text-zinc-400">
+            Some of the projects are from work and some are on my own time.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 "></div>
+        <div className="hidden w-full h-px md:block bg-zinc-800" />
+
+        <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 0)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={views[project.slug] ?? 0} />
+                </Card>
+              ))}
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 1)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={views[project.slug] ?? 0} />
+                </Card>
+              ))}
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {sorted
+              .filter((_, i) => i % 3 === 2)
+              .map((project) => (
+                <Card key={project.slug}>
+                  <Article project={project} views={views[project.slug] ?? 0} />
+                </Card>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
